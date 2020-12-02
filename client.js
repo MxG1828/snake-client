afe8bdca2b70d1cd350570eb685dde24ad93a72a
Michael Xiang <yorkmike4@gmail.com>
2020-12-02 22:20:45 +0000

    complete

--- client.js
+++ client.js
@@ -1,23 +1,23 @@
-const net = require('net');
+const net = require("net");
 const stdin = process.stdin;
-stdin.setEncoding('utf-8');
+stdin.setEncoding("utf-8");
 stdin.resume();
 
-const connect = function() {
+const connect = function () {
   const conn = net.createConnection({
     host: "135.23.222.131",
     port: 50542,
   });
-  conn.on("connect",() => {
-    console.log ("connected");
-    conn.write('Name: XBC');
+  conn.on("connect", () => {
+    console.log("connected");
+    conn.write("Name: XBC");
   });
 
-  conn.on("data",(data) =>{
+  conn.on("data", (data) => {
     console.log(data);
   });
-  
-  conn.setEncoding('utf8');
+
+  conn.setEncoding("utf8");
   return conn;
 };
-module.exports = {connect}
+module.exports = { connect };
--- input.js
+++ input.js
@@ -1,15 +1,30 @@
+let connection;
+
 const handleUserInput = (data) => {
-  if (data === '\u0003') {
+  if (data === "\u0003") {
     process.exit();
+  }
+  if (data === "w") {
+    connection.write("Move: up");
+  }
+  if (data === "s") {
+    connection.write("Move: down");
+  }
+  if (data === "a") {
+    connection.write("Move: left");
+  }
+  if (data === "d") {
+    connection.write("Move: right");
   }
 };
 
-const setupInput = function() {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
+  stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data",(data) => handleUserInput(data));
+  stdin.on("data", (data) => handleUserInput(data));
   return stdin;
-}
-module.exports = {setupInput}
+};
+module.exports = { setupInput };
